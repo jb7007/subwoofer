2365d5abd2384085a1069f9a48103e96caccf77a
Jeremiah Branch <jdbranch@mtu.edu>
2025-05-01 14:28:44 -0400

    designed front webpage and added basic css

--- app.py
+++ app.py
@@ -4,7 +4,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    user = "Jeremiah"
+    minutes = 145
+    logs = [
+        {"date": "Apr 30", "minutes": 25, "activity": "Scales & Arpeggios"},
+        {"date": "Apr 29", "minutes": 45, "activity": "Rhapsody in Blue"},
+        {"date": "Apr 28", "minutes": 30, "activity": "Chromatic Exercises"},
+    ]
+    return render_template("index.html", user=user, minutes=minutes, logs=logs)
 
 if __name__ == "__main__":
     app.run(debug=True)
