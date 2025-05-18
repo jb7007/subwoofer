085f8ca13d71fc969ffad0d0698b21e1968673cc
Jeremiah Branch <jdbranch@mtu.edu>
2025-05-18 08:50:54 -0400

    images

--- app.py
+++ app.py
@@ -2,16 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/home")
 def home():
+    return("index.html")
+
+@app.route("/dashboard")
+def dashboard():
     user = "Jeremiah"
     minutes = 145
-    logs = [
-        {"date": "Apr 30", "minutes": 25, "activity": "Scales & Arpeggios"},
-        {"date": "Apr 29", "minutes": 45, "activity": "Rhapsody in Blue"},
-        {"date": "Apr 28", "minutes": 30, "activity": "Chromatic Exercises"},
-    ]
-    return render_template("index.html", user=user, minutes=minutes, logs=logs)
+    return render_template("dashboard.html", user=user, minutes=minutes)
 
 @app.route("/log")
 def log_page():
