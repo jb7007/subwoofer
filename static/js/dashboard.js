4e9084473ce0734930f1000f8413d2877854a0bb
Jeremiah Branch <jdbranch@mtu.edu>
2025-06-24 14:42:49 -0400

    established front-end logic for dashboard for fetching and dynamically loading recent logs and user stats

---
+++ static/js/dashboard.js
@@ -0,0 +1,29 @@
+// static/js/dashboard.js
+import { setupModalListeners, renderRecentLogs } from "./logic.js";
+import { recentLogs } from "./api.js";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  setupModalListeners();
+
+  // Fetch and display recent logs on page load
+  try {
+    const { ok, data } = await recentLogs();
+    if (ok) renderRecentLogs(data);
+    else console.error("Failed to load recent logs.");
+  } catch (err) {
+    console.error("Error fetching recent logs:", err);
+  }
+
+  fetch("/api/dash-stats")
+  .then(response => {
+    if (!response.ok) throw new Error("Network response was not ok");
+    return response.json();
+  })
+  .then(data => {
+    document.getElementById("top-instrument").textContent = data.common_instrument;
+    document.getElementById("total-mins").textContent = data.total_minutes;
+    document.getElementById("total-mins-header").textContent = data.total_minutes;
+    document.getElementById("avg-mins").textContent = data.average_minutes;
+    document.getElementById("avg-mins-header").textContent = data.average_minutes;
+  })
+});
