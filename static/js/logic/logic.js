ee9c214521bd00982845d6ef2e0afc37f3691b3b
jb7007 <jdbranch@mtu.edu>
2025-07-19 18:58:09 -0400

    fix: deleted unused file

--- static/js/logic/logic.js
+++
@@ -1,8 +0,0 @@
-// static/js/logic.js
-// handles signup + log form logic, and sets up modal behavior
-
-import { instrumentMap } from "./utils.js"; // maps instrument codes to names
-
-// TODO: separate logic for setting up modal forms
-
-
