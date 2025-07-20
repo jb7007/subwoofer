d11b7feb2dafd1204e7ae9547a7ed550e35c67f8
jb7007 <jdbranch@mtu.edu>
2025-07-20 08:33:57 -0400

    feat: added function to safely close the log modal

---
+++ static/js/modals/modal-safety.js
@@ -0,0 +1,15 @@
+import { closeModal } from "./modal-helper.js";
+import { logAnimateModalOut } from "../../js/animation/modal.js";
+import { resetPieceComposerFields } from "../logic/log-form.js";
+
+export function closeLogModal() {
+	const modalEl = document.getElementById("logModal");
+	if (!modalEl) return;
+
+	if (logAnimateModalOut) {
+		resetPieceComposerFields();
+		logAnimateModalOut();
+	} else {
+		closeModal(modalEl);
+	}
+}
