6bd8bcb14e526427f8815ec435e6f3a76da16a05
jb7007 <jdbranch@mtu.edu>
2025-07-22 11:11:42 -0400

    refactor: move log logic and add index.js for all general logic

--- static/js/logic/logs/logs.js
+++ static/js/logic/logs/logs.js
@@ -2,19 +2,13 @@
 // SECTION: Log Data Functions
 // ====================
 
-import { submitLog } from "../../api/logs.js";
-import { closeLogModal } from "../../modals/modal-safety.js";
-import { extractPieceAndComposer } from "../../forms/log-form.js";
-import { setLogs } from "../../state/logs.js";
-import { renderLogs } from "../../components/log-table.js";
-import { fetchLogs } from "../../api/logs.js";
+import { extractPieceAndComposer } from "../../forms/index.js";
 
 // ANCHOR Sending Log Data
 
 export function getLogData() {
 	const { pieceTitle, composer } = extractPieceAndComposer();
 	const logData = {
-		local_date: document.getElementById("logDate").value.split("T")[0],
 		utc_timestamp: new Date().toISOString(),
 
 		duration: parseInt(document.getElementById("logDuration").value),
@@ -39,30 +33,4 @@ export function sortLogs(logs, field, ascending = true) {
 		if (valA > valB) return ascending ? 1 : -1;
 		return 0;
 	});
-}
-
-export async function handleLogSubmission() {
-	try {
-		const logData = getLogData();
-		console.log("Submitting log data:", logData);
-		const { ok, data } = await submitLog(logData);
-		console.log("Log submission response:", { ok, data });
-		console.log("Log data:", data);
-		if (!ok) {
-			alert(data.message || "Failed to submit log.");
-			return;
-		}
-
-		closeLogModal();
-
-		const { ok: updateOk, data: logs } = await fetchLogs();
-		console.log("Updated logs after submission:", logs);
-		if (updateOk) {
-			setLogs(logs);
-			renderLogs(logs);
-		}
-	} catch (error) {
-		console.error("Error submitting log:", error);
-		alert("An error occurred while submitting the log. Please try again.");
-	}
 }
