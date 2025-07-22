cef2ce3ec204a069aae74922096cca71462aa9f7
jb7007 <jdbranch@mtu.edu>
2025-07-22 17:46:49 -0400

    refactor: moved statistics logic to backend for frontend speed and debloating

--- app/routes/stats.py
+++ app/routes/stats.py
@@ -1,11 +1,11 @@
+from zoneinfo import ZoneInfo
 from flask import Blueprint, jsonify, render_template
 from flask_login import current_user, login_required
 from datetime import datetime
 
-from app.instrument_map import instrument_labels as INSTRUMENTS
 from app.models import PracticeLog
-from app.utils import (get_avg_log_mins, get_most_frequent, get_logs_from,
-                   get_total_log_mins)
+from app.utils import (get_avg_log_mins, get_most_frequent, get_logs_from, get_this_week_logs, get_today_log_mins,
+                   get_total_log_mins, get_intstrument_name, get_today_logs)
 
 stats_bp = Blueprint("stats", __name__)
 
@@ -15,15 +15,36 @@ def dashboard():
     date = datetime.now().strftime("%B %d, %Y")
     return render_template("dashboard.html", user=current_user, date=date)
 
-@stats_bp.route("/stats")
+@stats_bp.route("api/stats")
 @login_required
 def stats():
+    user = current_user
+    logs = get_logs_from(user)
+    weekly_logs = get_this_week_logs()
+    
+    total_minutes = get_total_log_mins(logs)
+    daily_mintes = get_today_log_mins(logs)
+    avg_minutes = get_avg_log_mins(logs, 2)
+    most_played_instrument = get_most_frequent(logs, attr="instrument")
+    
+    logs_with_instrument = PracticeLog.query.filter_by(
+        instrument=most_played_instrument
+    ).all()
+    
+    most_practiced_piece = get_most_frequent(
+        logs_with_instrument, attr="piece", weight_attr="duration", mode="value"
+    )
+    
+    piece_title = most_practiced_piece.title if most_practiced_piece else "Unlisted"
+    instrument_name = get_intstrument_name(most_played_instrument)
+    
     return render_template("stats.html")
 
 @stats_bp.route("/api/dash-stats", methods=["GET"])
 @login_required
 def dash_stats():
-    logs = get_logs_from(current_user)
+    user = current_user
+    logs = get_logs_from(user)
 
     if not logs:
         return (
@@ -46,18 +67,12 @@ def dash_stats():
     ).all()
     most_common_piece = get_most_frequent(freq_instr_logs, attr="piece", weight_attr="duration", mode="value")
     common_title = most_common_piece.title if most_common_piece else "Unlisted"
-    common_instrument = (
-        INSTRUMENTS.get(most_played_instrument, "Unlisted")
-        if most_played_instrument
-        else "Unlisted"
-    )
 
     return (
         jsonify(
             {
                 "total_minutes": total_minutes,
                 "average_minutes": avg_minutes,
-                "common_instrument": common_instrument,
                 "common_instr_key": most_played_instrument,
                 "common_piece": common_title,
             }
